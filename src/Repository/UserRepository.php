c5cd0a89fdf7516c5213899fec17a9e692bc5ca7
Josio <j.zafimanga@etechconsulting-mg.com>
2021-08-24 14:07:11 +0300

    account

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -12,7 +14,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
@@ -47,4 +49,20 @@ class UserRepository extends ServiceEntityRepository
         ;
     }
     */
+
+    /**
+     * @param string $username
+     *
+     * @return mixed|null|UserInterface
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function loadUserByUsername(string $username)
+    {
+        return $this->createQueryBuilder('u')
+                    ->andWhere('u.username = :val')
+                    ->andWhere('u.IsValid = 1')
+                    ->setParameter('val', $username)
+                    ->getQuery()
+                    ->getOneOrNullResult();
+    }
 }

8cf49238afc09316e3ac22498bca23cfab9df26e
matheos20 <morianthes04herbin@gmail.com>
2021-11-02 05:20:27 +0100

    add rechecher pdl et pec

--- src/Form/ElectriciteRechercheType.php
+++ src/Form/ElectriciteRechercheType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ElectriciteRecherche;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,11 +14,12 @@ class ElectriciteRechercheType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('PDL1',TextType::class, [
+            ->add('PDL1',SearchType::class, [
                 'required' => false,
                 'label' => false,
                 'attr' =>[
-                    'placeholder' => 'pdl'
+                    'class' => 'form-control border-end-0 border rounded-pill',
+                    'placeholder' => 'Recherche pdl'
                 ]
             ])
         ;
--- src/Form/GazRechercheType.php
+++ src/Form/GazRechercheType.php
@@ -6,6 +6,7 @@ use App\Entity\GazRecherche;
 use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,11 +16,12 @@ class GazRechercheType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('PCE1',TextType::class, [
+            ->add('PCE1',SearchType::class, [
                 'required' => false,
                 'label' => false,
                 'attr' =>[
-                    'placeholder' => 'pce'
+                    'class' => 'form-control border-end-0 border rounded-pill',
+                    'placeholder' => 'Recherche PCE'
                 ]
                 ])
         ;
